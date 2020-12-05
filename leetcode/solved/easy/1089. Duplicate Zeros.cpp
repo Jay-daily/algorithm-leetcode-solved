1bd236b4d7a52bc3fc8669a2e5b70887a01b5f26
Jay-daily <75241209+Jay-daily@users.noreply.github.com>
2020-12-06 03:26:20 +0900

    Create 1089. Duplicate Zeros.cpp

---
+++ leetcode/solved/easy/1089. Duplicate Zeros.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    void duplicateZeros(vector<int>& arr) {
+        for(int i =0; i< arr.size(); i++){
+            if(arr[i]==0 && i <arr.size()-2)
+            {
+                for(int j = arr.size()-1; j>=i+2; j--){
+                    arr[j] = arr[j-1];
+                }
+                arr[i+1]=0;
+                i++;
+            }
+            else if (arr[i]==0 && i ==arr.size()-2)
+            {
+                arr[i+1]=0;
+                i++;   
+            }
+        }
+    }
+};
