06c3b891153587d89bfa0524f84ec7cd8bd6d38c
Jay-daily <75241209+Jay-daily@users.noreply.github.com>
2020-12-18 16:43:05 +0900

    Create 1528. Shuffle String.cpp

---
+++ leetcode/solved/easy/1528. Shuffle String.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string restoreString(string s, vector<int>& indices) {
+        string res = "";
+        
+        for(int i = 0; i < s.size(); i++){
+            res += 'x';
+        }
+        
+        for(int i = 0; i < s.size(); i++){
+            for(int j = 0 ; j<s.size(); j++){
+                if(indices[j] == i)
+                {
+                    res[i] = s[j];
+                }
+            }
+        }
+        return res;
+    }
+};
