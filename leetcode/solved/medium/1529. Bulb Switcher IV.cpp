bb6dce6ec38337085846972cc463d3cd0b9f524f
Jay-daily <75241209+Jay-daily@users.noreply.github.com>
2021-01-10 20:31:19 +0900

    Create 1529. Bulb Switcher IV.cpp

---
+++ leetcode/solved/medium/1529. Bulb Switcher IV.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minFlips(string target) {
+        int n = target.size();
+        int flips = 0;
+        char status = '0';
+
+        for (int i=0; i<n; i++) {
+            if (status != target[i]) {
+                flips++;
+                status = status == '0' ?  '1' : '0';  
+            }
+        }
+        return flips;
+    }
+};
