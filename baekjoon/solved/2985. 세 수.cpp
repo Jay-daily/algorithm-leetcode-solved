86d7e5e65bb5c32388ef33265b534edacdbc1921
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-08-12 14:57:47 +0900

    Create 2985. 세 수.cpp

---
+++ baekjoon/solved/2985. 세 수.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main() {
+
+  
+    int a, b, c;
+    cin >> a >> b >> c;
+
+    if (a + b == c)
+    {
+        cout << a << "+" << b << "=" << c;
+
+    }
+    else if (b + c == a)
+    {
+        cout << a << "=" << b << "+" << c;
+
+    }
+    else if (a / b == c)
+    {
+        cout << a << "/" << b << "=" << c;
+
+    }
+    else if (b / c == a)
+    {
+        cout << a << "=" << b << "/" << c;
+
+    }
+    else if (a * b == c)
+    {
+        cout << a << "*" << b << "=" << c;
+    }
+    else if (b * c == a)
+    {
+        cout << a << "=" << b << "*" << c;
+    }
+    else if (a - b == c)
+    {
+        cout << a << "-" << b << "=" << c;
+    }
+    else if (b - c == a)
+    {
+        cout << a << "=" << b << "-" << c;
+    }
+    else
+        ;
+
+       
+    return 0;
+
+}
