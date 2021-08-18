6412cbfd107b761a768cd6a0a03db9884e58f459
shiGeumChi <gygygy711@knou.ac.kr>
2021-08-18 18:51:43 +0900

    d

--- jongmanbook/practice.cpp
+++ jongmanbook/practice.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <stdio.h>
 
-
 using namespace std;
 
 //1부터 n까지의 합을 계산하는 반복 함수와 재귀 함수
@@ -13,7 +12,6 @@ int sum(int n){
   for (int i = 1; i<=n; ++i){
     ret = ret +1;
   }
-
   return ret;
 }
 
@@ -25,9 +23,7 @@ int recursiveSum(int n){
 }
 
 
-
 int main (){
   cout<< sum(4) << endl << recursiveSum(3);
-
   return 1;
 }
