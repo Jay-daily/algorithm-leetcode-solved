4f4c49d4de2829a8f6b85364ee7d3cd52bcf36e1
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-09-16 10:41:11 +0900

    Update 1603. Design Parking System.cpp

--- leetcode/solved/easy/1603. Design Parking System.cpp
+++ leetcode/solved/easy/1603. Design Parking System.cpp
@@ -17,3 +17,30 @@ class ParkingSystem {
  * ParkingSystem* obj = new ParkingSystem(big, medium, small);
  * bool param_1 = obj->addCar(carType);
  */
+
+
+
+
+class ParkingSystem {
+    
+    public:
+    int carsize[3];
+    
+    ParkingSystem(int big, int medium, int small) {
+        this->carsize[0] = big;
+        this->carsize[1] = medium;
+        this->carsize[2] = small;
+
+    }
+    
+    bool addCar(int carType) {
+        return carsize[carType-1]-- > 0;
+    }
+    
+};
+
+/**
+ * Your ParkingSystem object will be instantiated and called as such:
+ * ParkingSystem* obj = new ParkingSystem(big, medium, small);
+ * bool param_1 = obj->addCar(carType);
+ */
