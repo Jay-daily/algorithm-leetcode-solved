2580c96ce9829fb58b73ec94706663b761452777
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-10-07 15:27:54 +0900

    Update 921. Minimum Add to Make Parentheses Valid.cpp

--- leetcode/solved/medium/921. Minimum Add to Make Parentheses Valid.cpp
+++ leetcode/solved/medium/921. Minimum Add to Make Parentheses Valid.cpp
@@ -8,3 +8,18 @@ public:
         return s.size();
     }
 };
+
+
+//better solution
+class Solution {
+public:
+    int minAddToMakeValid(string s) {
+        stack<char>stk;
+        for(int i=0; i<s.size();i++){
+            if(s[i]=='(') stk.push(s[i]);
+            else if(!stk.empty() && s[i]==')' && stk.top()=='(') stk.pop();
+            else stk.push('x');
+        }
+        return stk.size();
+    }
+};
