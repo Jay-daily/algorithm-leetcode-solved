6d73cc10c2fa88a5f166dd47eb2c93cabf4d236c
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-10-07 15:38:59 +0900

    Create 1304. Find N Unique Integers Sum up to Zero.cpp

---
+++ leetcode/solved/easy/1304. Find N Unique Integers Sum up to Zero.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> sumZero(int n) {
+        vector<int>res;
+        if(n%2==1)res.push_back(0);
+        for(int i =0; i<n/2;i++){
+            res.push_back(i+1);
+            res.push_back(-i-1);
+        }
+        return res;
+    }
+};
