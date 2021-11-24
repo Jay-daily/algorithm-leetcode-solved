31eacf084afc114c734649847009dd8a1e29b400
shiGeumChi <75241209+shiGeumChi@users.noreply.github.com>
2021-11-24 15:44:54 +0900

    Create 278. First Bad Version.cpp

---
+++ leetcode/solved/easy/278. First Bad Version.cpp
@@ -0,0 +1,10 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        for(int i =n; i>0; i--) if(isBadVersion(i)==false) return i+1;
+        return 1;
+    }  
+};
